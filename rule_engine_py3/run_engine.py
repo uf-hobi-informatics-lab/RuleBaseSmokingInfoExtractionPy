4584a0dffc8759e51860cc93e735260ab106db2a
Daniel Paredes <108016828+dparedespardo@users.noreply.github.com>
2025-02-03 18:38:48 -0500

    Update run_engine.py to latest
    Including missing import statements and minor adjustments

--- rule_engine_py3/run_engine.py
+++ rule_engine_py3/run_engine.py
@@ -14,9 +14,11 @@ from TemporalExpression import *
 def teFilter(te):
 	# Remove concepts start at the same position. Will keep longer one if the same start position
     # Unless expressions are of different types
+    
     te_max = {}
     te_available = [0] * len(te)
     for i in range(0, len(te)):
+        #print(te[i].text, te[i].start, te[i].end)
         if ( str(te[i].start)) not in te_max:
             te_max[ str(te[i].start)] = [0]*3
             te_max[ str(te[i].start)][0] = te[i].end
@@ -42,7 +44,7 @@ def teFilter(te):
     for i in range(0, len(te)):
         if te_available[i] == 1:
             new_te.append(te[i])
-	
+
     return new_te
 
 
@@ -50,33 +52,18 @@ def teClean(te):
     new_te = []
     for expr in te:
         data = re.split('\s+', expr.value)
-        if (len(data) < 2):
-            placehold_value = data.pop(0)
-            if (placehold_value[-3:]!='PPD'):
-                stop = -2
-            else:
-                stop = -3
-            expr.type = placehold_value[stop:]
-            expr.value = placehold_value[0:stop]
-        elif   (len(data) > 2):
-            expr.type = data[-1]
-            expr.value = ' '.join(str(item) for item in data[:-1])
-        else:
-            if (data[0] == ''):
-                split = int(len(data[1])/2)
-                placehold_data = data.pop(1)
-                expr.type = placehold_data[split:]
-                expr.value = placehold_data[0:split]
-            else:
-                expr.type = data[-1]
-                expr.value = data[0]
-        match = re.search(expr.value, expr.text) 
-        expr.start = expr.start + match.start()
+        expr.value = ' '.join(list(data[0:-1]))
+        expr.type = data[-1]
+
+        match = expr.text.rfind(expr.value)
+
+        expr.start = expr.start + match#.start()
         expr.end = expr.start + len(expr.value)
        
     
     te_start = {}
     te_available = [0] * len(te)
+    
     for i in range(0, len(te)):
         if (te[i].start not in te_start):
             te_start[ te[i].start] = [0]*2
@@ -86,14 +73,11 @@ def teClean(te):
         else:
             if ((len(te[i].text) < len(te[te_start[te[i].start][1]].text)) & 
                   (te[i].start == te_start[ te[i].start][0])):
-                # print(f"[NOTICE]:\tKeeping [{te[te_start[te[i].start][1]].text}] over [{te[i].text}]")
                 te_available[te_start[te[i].start][1]] = 1
                 te_available[i] = 0
-            elif ((te[i].start == te_start[ te[i].start][0])): ## Change this
-                # print(f"[NOTICE]:\tRemoving [{te[te_start[te[i].start][1]].text}] in favor of [{te[i].text}]")
+            elif ((te[i].start == te_start[ te[i].start][0])): 
                 te_available[te_start[te[i].start][1]] = 0
                 te_available[i] = 1
-
     for i in range(0, len(te)):
         if te_available[i] == 1:
             new_te.append(te[i])
@@ -104,25 +88,54 @@ def teClean(te):
 def teFormat(te, csv=False):
     new_te = []
     ct = 1
+
     if csv == True:
-		categories, values = [], []
+        categories, values = [], []
         for expr in te:
-			category, value = expr.type, expr.value
+            category, value = expr.type, expr.value
             categories.append(category)
             values.append(value)
         return categories, values
     else:
         for expr in te:
-	    formattedExpr = f"T{ct}\t{expr.type} {expr.start} {expr.end}\t{expr.value}"
-	    ct += 1
-			new_te.append(formattedExpr)
-	return new_te
+            
+            if (expr.value.find(' ')!=-1):
+                scores = expr.value.split(' ')
+                start = expr.start
+                end = expr.end
+                for elem in scores:
+                    #print(elem)
+                    end = start + len(elem)
+                    formattedExpr = f"T{ct}\t{expr.type} {start} {end}\t{elem}"
+                    start = end + 1
+                    ct += 1
+                    new_te.append(formattedExpr)
+            elif (expr.value.find('//')!=-1):
+                scores = expr.value.split('//')
+                start = expr.start
+                end = expr.end
+                for elem in scores:
+                    end = start + len(elem)
+                    formattedExpr = f"T{ct}\t{expr.type} {start} {end}\t{elem}"
+                    start = end + 2
+                    ct += 1
+                    new_te.append(formattedExpr)
+            else:
+                formattedExpr = f"T{ct}\t{expr.type} {expr.start} {expr.end}\t{expr.value}"
+                new_te.append(formattedExpr)
+            ct += 1
+            print(formattedExpr)
+        
+    return new_te
 
 
 if __name__ == "__main__":
     import sys
     import glob
     import re
+    import csv
+    import pandas as pd
+
 
     ''' 1. input text '''
     my_engine= RuleEngine()
@@ -134,13 +147,17 @@ if __name__ == "__main__":
             myf.close()
             
             results=my_engine.extract(text)
+            #print ("total %s results" % str(len(results))  )
             results = teFilter(results)
 
             results = teClean(results)
 
             print ("total %s results after filtering" % str(len(results))  )
+            for each in results:
+                print(each.type, each.value)
 
             results = teFormat(results)
+            print(results)
 
             print ("\n\n-----")
             output_filename = file[:-4] + ".ann"
@@ -176,5 +193,5 @@ if __name__ == "__main__":
             except:
                 continue
         print(f'Saving output file to {str(sys.argv[1])[:-4]}_output_file.csv')
-        df.to_csv(f'{str(sys.argv[1])[:-4]}_output_file.csv', index=False, quoting=csv.QUOTE_ALL)
+        df.to_csv(f'{str(sys.argv[1])[:-4]}_output_file.tsv', index=False, quoting=csv.QUOTE_ALL, sep='\t')
             
